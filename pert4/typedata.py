b3d280f2515fa9591ca5b17bca47fe153b6090f8
dwisanjayarizky <dwisanjayarizky@gmail.com>
2022-10-18 10:43:42 +0700

    sesi4

--- pert4/typedata.py
+++ pert4/typedata.py
@@ -1,42 +1,118 @@
-#ctype_data_int
-pertama = 10
-print(pertama)
-print(type(pertama))
+print ("===Tipe Data String===")
+print("===Tipe Data String===")
+a = "hello"
+print (type(a),"\n")
+#contoh string
+print(type(a), "\n")
+# contoh string
+a_string = "Hallo, mamang"
+print (a_string)
+print(a_string)
 
-#type_data_float
-kedua = 1.5
-print(kedua)
-print(type(kedua))
+print("===Tipe Data Integer===") 
+print("===Tipe Data Integer===")
+b = 12
+print (type(b),"\n")
+#contoh integer
+b_int = (1,4,6,7)
+print (b_int)
+print(type(b), "\n")
+# contoh integer
+b_int = (1, 4, 6, 7)
+print(b_int)
 
-#type_data_tuple
-ketiga = 6, 9
-print(ketiga)
-print(type(ketiga))
+print("===Tipe Data Float===") 
+print("===Tipe Data Float===")
+c = 10.5
+print(type(c),"\n")
+#contoh float
+c_float =(10.5,12,3)
+print(type(c), "\n")
+# contoh float
+c_float = (10.5, 12, 3)
+print(c)
+print("===Tipe Data Tuple===") 
+d = (20.5,4)
+print(type(d),"\n")
+#contoh tuple
+tuple_f = (1,2,3,4.4)
+print (tuple_f)
+y = tuple ("a")
+print (y)
+print("===Tipe Data Tuple===")
+d = (20.5, 4)
+print(type(d), "\n")
+# contoh tuple
+tuple_f = (1, 2, 3, 4.4)
+print(tuple_f)
+y = tuple("a")
+print(y)
 
-#type_data_String
-keempat = "ayo berangkat"
-print(keempat)
-print(type(keempat))
+print("===Tipe Data Complex===") 
+print("===Tipe Data Complex===")
+e = 1j
+print(type(e), "\n")
+#contoh complex
+z = complex ('5-9j')
+# contoh complex
+z = complex('5-9j')
+print(z)
 
-#type_data_boolean
-angka1 = 12
-angka2 = 15
-penjumlahan = angka1 + angka2
-pengurangan  = angka1 - angka2
-print(penjumlahan < pengurangan)
-print(type(penjumlahan < pengurangan))
+print("===Tipe Data List===") 
+print("===Tipe Data List===")
+f = ["a", "b", "c"]
+print(type(f),"\n")
+#contoh list
+list_f = [1,2,3,"Imam",4,"Mulyana"]
+print (list_f)
+print(type(f), "\n")
+# contoh list
+list_f = [1, 2, 3, "Imam", 4, "Mulyana"]
+print(list_f)
 
-#type_data_complex
-B2 = complex(4, 5)
-print(B2)
-print(type(B2))
+print("===Tipe Data Set===") 
+print("===Tipe Data Set===")
+g = {"a", "b", "c"}
+print(type(g),"\n")
+print(type(g), "\n")
 
-#type_data_list
-Al = ([1,2,5,6])
-print(Al)
-print(type(Al))
+#contoh set
+set_f = [1,2,3]
+print (set_f[0])
+# contoh set
+set_f = [1, 2, 3]
+print(set_f[0])
+g_set = {"a", "b", "c"}
+print (g_set)
+print(g_set)
 
-#type_data_dictionary
-Ping = {"nama":"Alexa", 'kelas': 12}
-print(Ping)
-print(type(Ping))
+print("===Tipe Data Frozenset===") 
+h = frozenset ({1,2,3})
+print (type(h),"\n")
+print("===Tipe Data Frozenset===")
+h = frozenset({1, 2, 3})
+print(type(h), "\n")
+
+#contoh frozenset
+h = frozenset ({1,2,3})
+h = frozenset ("Imam")
+print (h)
+# contoh frozenset
+h = frozenset({1, 2, 3})
+h = frozenset("Imam")
+print(h)
+
+print("===Tipe Data Boolean===") 
+print("===Tipe Data Boolean===")
+i = True
+j = False
+print (type(i))
+print (type(j),"\n")
+#contoh boolean
+print(type(i))
+print(type(j), "\n")
+# contoh boolean
+i = True
+j = False
+print (i, j)
+print(i, j)
