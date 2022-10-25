da8999d931ef12bea280d8af176aa2ad2945137f
dwisanjayarizky <dwisanjayarizky@gmail.com>
2022-10-25 12:02:10 +0700

    materi_sesi1

---
+++ materi_sesi1/dasar.py
@@ -0,0 +1,21 @@
+def fungsi_pertama():
+    print("Halo dengan fungsi")
+
+def fungsi_kedua(fname):
+    print(fname + " Sundoro")
+
+def kali_lima(x):
+    return 5 * x
+
+# Halo dengan fungsi
+fungsi_pertama()
+
+# Hendra Sundoro
+fungsi_kedua("Hendra")
+# Tobi Sundoro
+fungsi_kedua("Tobi")
+# Lina Sundoro
+fungsi_kedua("Lina")
+
+# 320
+print(kali_lima(64))
