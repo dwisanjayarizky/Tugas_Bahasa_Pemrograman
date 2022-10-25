021672593bed45ae9f932c25547187a6b12f80d3
dwisanjayarizky <dwisanjayarizky@gmail.com>
2022-10-25 12:14:24 +0700

    materi_sesi4

---
+++ materi_sesi4/type-data.py
@@ -0,0 +1,118 @@
+print ("===Tipe Data String===")
+print("===Tipe Data String===")
+a = "hello"
+print (type(a),"\n")
+#contoh string
+print(type(a), "\n")
+# contoh string
+a_string = "Hallo, mamang"
+print (a_string)
+print(a_string)
+
+print("===Tipe Data Integer===") 
+print("===Tipe Data Integer===")
+b = 12
+print (type(b),"\n")
+#contoh integer
+b_int = (1,4,6,7)
+print (b_int)
+print(type(b), "\n")
+# contoh integer
+b_int = (1, 4, 6, 7)
+print(b_int)
+
+print("===Tipe Data Float===") 
+print("===Tipe Data Float===")
+c = 10.5
+print(type(c),"\n")
+#contoh float
+c_float =(10.5,12,3)
+print(type(c), "\n")
+# contoh float
+c_float = (10.5, 12, 3)
+print(c)
+print("===Tipe Data Tuple===") 
+d = (20.5,4)
+print(type(d),"\n")
+#contoh tuple
+tuple_f = (1,2,3,4.4)
+print (tuple_f)
+y = tuple ("a")
+print 
+print("===Tipe Data Tuple===")
+d = (20.5, 4)
+print(type(d), "\n")
+# contoh tuple
+tuple_f = (1, 2, 3, 4.4)
+print(tuple_f)
+y = tuple("a")
+print
+
+print("===Tipe Data Complex===") 
+print("===Tipe Data Complex===")
+e = 1j
+print(type(e), "\n")
+#contoh complex
+z = complex ('5-9j')
+# contoh complex
+z = complex('5-9j')
+print(z)
+
+print("===Tipe Data List===") 
+print("===Tipe Data List===")
+f = ["a", "b", "c"]
+print(type(f),"\n")
+#contoh list
+list_f = [1,2,3,"Imam",4,"Mulyana"]
+print (list_f)
+print(type(f), "\n")
+# contoh list
+list_f = [1, 2, 3, "Imam", 4, "Mulyana"]
+print(list_f)
+
+print("===Tipe Data Set===") 
+print("===Tipe Data Set===")
+g = {"a", "b", "c"}
+print(type(g),"\n")
+print(type(g), "\n")
+
+#contoh set
+set_f = [1,2,3]
+print (set_f[0])
+# contoh set
+set_f = [1, 2, 3]
+print(set_f[0])
+g_set = {"a", "b", "c"}
+print (g_set)
+print(g_set)
+
+print("===Tipe Data Frozenset===") 
+h = frozenset ({1,2,3})
+print (type(h),"\n")
+print("===Tipe Data Frozenset===")
+h = frozenset({1, 2, 3})
+print(type(h), "\n")
+
+#contoh frozenset
+h = frozenset ({1,2,3})
+h = frozenset ("Imam")
+print (h)
+# contoh frozenset
+h = frozenset({1, 2, 3})
+h = frozenset("Imam")
+print(h)
+
+print("===Tipe Data Boolean===") 
+print("===Tipe Data Boolean===")
+i = True
+j = False
+print (type(i))
+print (type(j),"\n")
+#contoh boolean
+print(type(i))
+print(type(j), "\n")
+# contoh boolean
+i = True
+j = False
+print (i, j)
+print(i, j)
